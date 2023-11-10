fb4365e4b635827b8c80de4a9f266c96452f3c2f
Gamaliel Burgos <54601555+Gamygams1234@users.noreply.github.com>
2023-11-09 16:48:27 -0800

    adding many tweets

--- app.js
+++ app.js
@@ -9,13 +9,56 @@ const db = mongoose.connection
 mongoose.connect(mongoURI, ()=>{
     console.log("Mongoose connection is established")
 })
-const myFirstTweet = {
-    title: 'Deep Thoughts',
-    body: 'Friends, I have been navel-gazing',
-    author: 'Karolin'
-  }
+const manyTweets = [
+    {
+      title: 'Deep Thoughts',
+      body: 'Friends, I have been navel-gazing',
+      author: 'Karolin'
+    },
+    {
+      title: 'Sage Advice',
+      body: 'Friends, I am vegan and so should you',
+      author: 'Karolin',
+      likes: 20
+    },
+    {
+      title: 'Whole Reality',
+      body: 'I shall deny friendship to anyone who does not exclusively shop at Whole Foods',
+      author: 'Karolin',
+      likes: 40
+    },
+    {
+      title: 'Organic',
+      body: 'Friends, I have spent $2300 to be one of the first people to own an organic smartphone',
+      author: 'Karolin',
+      likes: 162
+    },
+    {
+      title: 'Confusion',
+      body: 'Friends, why do you just respond with the word `dislike`? Surely you mean to click the like button?',
+      author: 'Karolin',
+      likes: -100
+    },
+    {
+      title: 'Vespa',
+      body: 'Friends, my Vespa has been upgraded to run on old french fry oil. Its top speed is now 11 mph',
+      author: 'Karolin',
+      likes: 2
+    },
+    {
+      title: 'Licensed',
+      body: 'Friends, I am now officially licensed to teach yogalates. Like this to get 10% off a private lesson',
+      author: 'Karolin',
+      likes: 3
+    },
+    {
+      title: 'Water',
+      body: 'Friends, I have been collecting rain water so I can indulge in locally sourced raw water. Ask me how',
+      author: 'Karolin',
+    },
+  ]
 
-  Tweet.create(myFirstTweet , (error, tweet) => {
+  Tweet.insertMany(manyTweets , (error, tweet) => {
     if (error) { // if there is an error console log it
       console.log(error)
     } else { // else show us the created tweet
